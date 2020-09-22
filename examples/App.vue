c40914473a51456d7ab59ddb71b98d786ff81419
lanshikeng@unnova.com <lanshikeng@unnova.com>
2020-09-22 17:34:08 +0800

    fix:segregate dialog and tabs style

--- examples/App.vue
+++ examples/App.vue
@@ -1,67 +1,5 @@
 <template>
-  <!--  <img alt="Vue logo" src="./assets/lui.png"/>-->
-  <div>{{ switchValue }}</div>
-  <l-radio @change="radioChange">默认</l-radio>
-  <l-radio>备选项</l-radio>
-  <l-switch
-      v-model:value="switchValue"
-      @change="handledChang"
-      :show-name="['开','关']"></l-switch>
-  <div ref="aa" class="a-block-box">
-    <div class="block-box">
-      <l-tabs style="width: 500px" v-model:value="activeName" @tabsClick="handledTabsClick">
-        <l-tab-item label="表情1" name="1">
-          <p>
-            Single-origin coffee tilde craft beer organic wolf. Plaid kickstarter vegan roof party vice, try-hard
-            taxidermy truffaut small batch. PBR&B butcher helvetica, green juice craft beer kickstarter skateboard. +1
-            selvage VHS gastropub slow-carb, twee vegan helvetica farm-to-table marfa seitan letterpress narwhal squid
-            cliche. Meggings fap leggings slow-carb kickstarter, health goth gastropub hashtag blue bottle photo booth
-            literally PBR&B iPhone ugh. Organic scenester truffaut pug, pitchfork man bun artisan brooklyn photo booth.
-            Biodiesel farm-to-table 8-bit direct trade humblebrag, man braid blue bottle.
-          </p>
-        </l-tab-item>
-        <l-tab-item label="标签2" name="2">
-          <p>cliche. Meggings fap leggings slow-carb kickstarter, health goth gastropub hashtag blue bottle photo booth
-            literally PBR&B iPhone ugh. Organic scenester truffaut pug, pitchfork man bun artisan brooklyn photo booth.
-            Biodiesel farm-to-table 8-bit direct trade humblebrag, man braid blue bottle.</p>
-          <l-button theme="success" icon="iconfont icon-d" @click="handledClick" circle></l-button>
-          <l-button theme="error" icon="iconfont icon-d" @click="handledClick" circle></l-button>
-          <l-button theme="warning" icon="iconfont icon-bianji1" @click="handledClick" circle></l-button>
-          <l-button theme="primary" icon="iconfont icon-tubiaozhizuomoban" @click="handledClick" circle></l-button>
-          <l-button theme="subordination" icon="iconfont icon-jigui" @click="handledClick" circle></l-button>
-          <l-button theme="dark" icon="iconfont icon-shebeijieruwangguanxinxi" @click="handledClick" circle></l-button>
-        </l-tab-item>
-        <l-tab-item label="标签3" name="3">
-          <p>
-            不得不承认，computed这个强大功能的实现果然少不了内部非常复杂的实现，这个双向依赖收集的套路相信也会给各位小伙伴带来很大的启发。跟着尤大学习，果然有肉吃！
-            另外由于@vue/reactivity的框架无关性，我把它整合进了React，做了一个状态管理库，可以完整的使用上述的computed等强大的Vue3能力。
-          </p>
-        </l-tab-item>
-      </l-tabs>
-    </div>
-  </div>
-  <div>
-    <l-button theme="success" icon="iconfont icon-d" @click="handledClick" circle></l-button>
-    <l-button theme="error" icon="iconfont icon-d" @click="handledClick" circle></l-button>
-    <l-button theme="warning" icon="iconfont icon-bianji1" @click="handledClick" circle></l-button>
-    <l-button theme="primary" icon="iconfont icon-tubiaozhizuomoban" @click="handledClick" circle></l-button>
-    <l-button theme="subordination" icon="iconfont icon-jigui" @click="handledClick" circle></l-button>
-    <l-button theme="dark" icon="iconfont icon-shebeijieruwangguanxinxi" @click="handledClick" circle></l-button>
-  </div>
-  <div>{{ a }}</div>
-  <br>
-<!--  <l-dialog :headerIcon="['iconfont icon-d','#f60']" title="创建组件">-->
-<!--    <l-button theme="success" icon="iconfont icon-d" @click="handledClick" circle></l-button>-->
-<!--    <l-button theme="error" icon="iconfont icon-d" @click="handledClick" circle></l-button>-->
-<!--    <l-button theme="warning" icon="iconfont icon-bianji1" @click="handledClick" circle></l-button>-->
-<!--    <l-button theme="primary" icon="iconfont icon-tubiaozhizuomoban" @click="handledClick" circle></l-button>-->
-<!--    <l-button theme="subordination" icon="iconfont icon-jigui" @click="handledClick" circle></l-button>-->
-<!--    <l-button theme="dark" icon="iconfont icon-shebeijieruwangguanxinxi" @click="handledClick" circle></l-button>-->
-<!--    <template v-slot:footer>-->
-<!--      <l-button theme="error" icon="iconfont icon-d" @click="handledClick" circle></l-button>-->
-<!--      <l-button theme="success" icon="iconfont icon-d" @click="handledClick" circle></l-button>-->
-<!--    </template>-->
-<!--  </l-dialog>-->
+
 </template>
 <script>
 
@@ -72,31 +10,9 @@ export default {
 
 
   setup(props, context) {
-    let a = ref(1)
-    let activeName = ref('1')
-    let switchValue = ref(false)
-    const handledClick = (e) => {
-      a.value++
-      // console.log(a);
-    };
-    const handledChang = (val, e) => {
-      // console.log(val);
 
-    }
-    const radioChange = (val) => {
-      console.log(val);
-    }
-    const handledTabsClick = (val, name) => {
-      // console.log(name);
-    }
     return {
-      handledClick,
-      a,
-      switchValue,
-      handledChang,
-      radioChange,
-      activeName,
-      handledTabsClick
+
     }
   }
 
--- src/packages/dialog/src/dialog.vue
+++ src/packages/dialog/src/dialog.vue
@@ -1,6 +1,7 @@
 <template>
-  <transition>
-    <div class="l-dialog-box" :style="{backgroundColor:modal?'rgba(0, 0, 0, .6)':''}">
+  <transition name="dialog-fade">
+    <div v-show="visible" @click.self="close" class="l-dialog-box"
+         :style="{backgroundColor:modal?'rgba(0, 0, 0, .6)':''}">
       <div class="l-dialog-main" :style="computedMainSize">
         <header v-if="showHeader" class="l-dialog-header">
           <span><slot name="header">{{ title || 'Dialog' }}</slot></span>
@@ -18,7 +19,7 @@
 </template>
 
 <script>
-import {defineComponent, computed} from "vue"
+import {defineComponent, computed, toRefs} from "vue"
 
 export default defineComponent(
     {
@@ -36,76 +37,35 @@ export default defineComponent(
           type: Boolean,
           default: true
         },
-        headerIcon: [String, Array]
+        headerIcon: [String, Array],
+        visible: Boolean,
+        clickModalClose: {
+          type: Boolean,
+          default: true
+        }
       },
       setup(props, context) {
+        const {clickModalClose} = toRefs(props);
         const computedMainSize = computed(() => {
           const style = {};
           style.width = props.width || 30 + '%';
           style.marginTop = props.top || 15 + "vh";
           return style
         })
+        const close = () => {
+          if (clickModalClose.value) context.emit('update:visible', false)
+        }
         // const computedHeaderIcon = computed()
         //todo  支持自定义header icon
         if (Array.isArray(props.headerIcon)) {
 
         }
         return {
-          computedMainSize
+          computedMainSize,
+          close,
+          clickModalClose
         }
       }
     },
 )
 </script>
-<style>
-.l-dialog-box {
-  overflow: auto;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 9999;
-  background-color: rgba(0, 0, 0, 0.2);
-}
-
-.l-dialog-main {
-  position: relative;
-  margin: 0 auto 50px;
-  background: #fff;
-  border-radius: 2px;
-  /*box-shadow: 0 1px 3px rgba(0, 0, 0, .3);*/
-  box-shadow: 0 5px 5px -3px rgba(0, 0, 0, .2), 0 8px 10px 1px rgba(0, 0, 0, .14), 0 3px 14px 2px rgba(0, 0, 0, .12)
-
-}
-
-.l-dialog-footer {
-  padding: 10px 15px 10px;
-  text-align: right;
-}
-
-.l-dialog-header {
-  position: relative;
-  font-size: 16px;
-  color: #333;
-  padding: 10px 15px 10px;
-}
-
-/*.l-dialog-header:before{*/
-/*  position: absolute;*/
-/*  bottom: 0;*/
-/*  content: '';*/
-/*  height: 1px;*/
-/*  width: 90%;*/
-/*  background-color: rgba(197, 197, 197, 0.3);*/
-/*}*/
-.l-dialog-body {
-  padding: 16px;
-}
-
-.l-dialog-close {
-  font-size: 12px;
-  color: #7f8280;
-  text-align: right;
-}
-</style>
