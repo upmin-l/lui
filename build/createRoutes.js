3a4a727f137d602bfcc51e6f7a79abb27a2b03eb
upmin-l <904412200@qq.ocm>
2021-11-03 14:30:17 +0800

    fix: examples routes

--- build/createRoutes.js
+++ build/createRoutes.js
@@ -1,5 +1,30 @@
 const fs = require('fs')
 const path = require('path')
+const renderString = require('json-templater/string');
+
+const MAIN_TEMPLATE=`
+export const zhComponentRoutes = [
+    {{componentsPath}}
+];
+export const routes = [
+    {
+        path: '/',
+        redirect: 'home'
+    },
+    {
+        name: 'home',
+        path: '/home',
+        component: () => import('../views/site/home.vue'),
+        children:zhComponentRoutes
+    }
+]
+`
+const ROUTE_PATH = `
+{
+path: '{{path}}',
+component: () => import('{{component}}')
+}`
+
 
 class CreateRoutes {
     constructor() {
@@ -22,13 +47,28 @@ class CreateRoutes {
 
     processStart(is_dev = true) {
         if (is_dev) {
-            const componentPath = path.resolve(__dirname, '..', 'src/packages/');
+            const componentPath = path.resolve(__dirname, '../src/packages/');
+            let componentsPath = []
             fs.readdirSync(componentPath).forEach((item) => {
                 const url = path.resolve(componentPath, item);
                 const state = fs.statSync(url);
                 if (state.isDirectory()) {
-                    console.log(item);
+                    componentsPath.push(renderString(ROUTE_PATH, {
+                        path: item,
+                        component: `../demo/${ item }/index.entry.md`
+                    }))
                 }
+            })
+            const routePath = path.resolve(__dirname, '../examples/routes/route.js')
+            const template = renderString(MAIN_TEMPLATE, {
+                componentsPath: componentsPath.join(',')
+            })
+            fs.writeFile(routePath, template, (err) => {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                console.log('ok')
             })
         }
     }
--- examples/routes/index.js
+++ examples/routes/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 export default function (app, routes = []) {
+    console.log(routes);
     const router = createRouter({
         history: createWebHistory(),
         routes
     })
     router.beforeEach((to, from, next) => {
-
+        next()
     })
     router.afterEach((to, from, next) => {
 
