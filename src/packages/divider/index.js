1c7bac6cc8c888313e428df71fd65eeae4cec3a0
upmin-l <904412200@qq.ocm>
2021-11-15 18:03:33 +0800

    fix: divider components basics

---
+++ src/packages/divider/index.js
@@ -0,0 +1,6 @@
+import divider from './src/divider.vue';
+divider.install = function (Vue) {
+Vue.component(divider.name, divider)
+};
+export default divider
+                            
---
+++ src/packages/divider/src/divider.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="getStyle" class="l-divider">
+    <div :class="['l-divider-content',`is_${ position }`]" v-bind="$attrs">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { computed } from "vue";
+
+export default {
+  name: "lDivider",
+  props: {
+    position: {
+      type: String,
+      default: 'left',
+      validator(val) {
+        return ['left', 'center', 'right'].includes(val.value)
+      }
+    },
+    theme: {
+      type: String,
+      default: 'default'
+    },
+  },
+  setup(props) {
+    const getStyle = computed(() => {
+      return [
+        `l-divider-${ props.theme }`,
+      ]
+    })
+    return {
+      getStyle
+    }
+  }
+}
+</script>
